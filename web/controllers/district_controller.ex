7f08a8078f211bc2a6efe7b50e88b3af03f15b3b
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-08-07 16:40:34 -0400

    add endpoint to get representatives

--- web/controllers/district_controller.ex
+++ web/controllers/district_controller.ex
@@ -1,18 +1,21 @@
 defmodule KratosApi.DistrictController do
   use KratosApi.Web, :controller
 
-  require IEx
-
   plug :scrub_params, "address" when action in [:post]
 
-  def post(conn, params) do
-    response = HTTPotion.get Application.get_env(:kratos_api, :geocodio)[:api_url],
-      query: %{
-        q: params["address"],
-        api_key: Application.get_env(:kratos_api, :geocodio)[:api_key],
-        fields: "cd"
-      }
+  def post(conn, %{"address" => address}) do
+    [{_, api_key},{_, api_url}] = Application.get_env(:kratos_api, :geocodio)
+    response = HTTPotion.get api_url,
+      query: %{q: address, api_key: api_key, fields: "cd"}
+
     json conn, Poison.decode! response.body
+  end
+
+  def show(conn, %{"state" => state, "id" => id}) do
+    representatives = Enum.filter(
+      Govtrack.roles([current: true, state: state]).body["objects"],
+      fn(x) -> x["district"] == String.to_integer(id) || x["role_type"] == "senator" end)
+    json conn, representatives
   end
 
 end
