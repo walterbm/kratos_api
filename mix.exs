5fa8beed3abe04705cf9f50a3ce4b1293f2649a6
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-08-07 18:31:03 -0400

    add exrm to prepare deployment releases

--- mix.exs
+++ mix.exs
@@ -53,6 +53,7 @@ defmodule KratosApi.Mixfile do
       {:cowboy, "~> 1.0"},
       {:govtrack, "~> 0.5.0"},
       {:httpotion, "~> 3.0.0"},
+      {:exrm, "~> 1.0.8"},
       {:edeliver, "~> 1.3.0"}
     ]
   end
