a7a86065af48f0ff9b3c415e49119ff022b5b728
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-08-23 23:19:02 -0400

    filter out votes for clotures and procedural motions

--- web/controllers/representative_controller.ex
+++ web/controllers/representative_controller.ex
@@ -5,6 +5,8 @@ defmodule KratosApi.RepresentativeController do
     voting_record =
     Govtrack.vote_voters([person: id, order_by: "-created", limit: 100]).body["objects"]
     |> Enum.map(fn(x) -> Map.merge(x["option"], x["vote"]) end)
+    |> Enum.filter(fn(x) -> x["category"] != "cloture" end)
+    |> Enum.filter(fn(x) -> x["category"] != "procedural" end)
     json conn, voting_record
   end
 
