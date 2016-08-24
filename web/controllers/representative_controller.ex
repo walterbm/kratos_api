ba9571658ae2729e342cdced14bf6b3e8bb1bb85
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-08-23 22:12:32 -0400

    bump representatives vote results to 100

--- web/controllers/representative_controller.ex
+++ web/controllers/representative_controller.ex
@@ -3,7 +3,7 @@ defmodule KratosApi.RepresentativeController do
 
   def show(conn, %{"id" => id}) do
     voting_record =
-    Govtrack.vote_voters([person: id, order_by: "-created", limit: 20]).body["objects"]
+    Govtrack.vote_voters([person: id, order_by: "-created", limit: 100]).body["objects"]
     |> Enum.map(fn(x) -> Map.merge(x["option"], x["vote"]) end)
     json conn, voting_record
   end
