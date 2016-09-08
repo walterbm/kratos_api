cf5794e7e437f4ecb7aef11baa3b6c0e4d68b7ef
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-09-08 02:08:25 -0400

    add registration and session routes & controllers

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,28 @@
+defmodule KratosApi.SessionController do
+  use KratosApi.Web, :controller
+
+  plug :scrub_params, "session" when action in [:create]
+
+  def create(conn, %{"session" => session_params}) do
+    case KratosApi.Session.authenticate(session_params) do
+      {:ok, user} ->
+        {:ok, jwt, _full_claims} = user |> Guardian.encode_and_sign(:token)
+
+        conn
+        |> put_status(:created)
+        |> render("show.json", jwt: jwt, user: user)
+
+      :error ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render("error.json")
+    end
+  end
+
+  def unauthenticated(conn, _params) do
+    conn
+    |> put_status(:forbidden)
+    |> render(KratosApi.SessionView, "forbidden.json", error: "Not Authenticated")
+  end
+
+end
