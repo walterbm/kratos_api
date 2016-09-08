54e2a8707db387da27aec43ba484bc6998db8b66
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-09-08 02:09:18 -0400

    add authentication and password check helpers

---
+++ web/helpers/session.ex
@@ -0,0 +1,20 @@
+defmodule KratosApi.Session do
+  alias KratosApi.{Repo, User}
+
+  def authenticate(%{"phone" => phone, "password" => password}) do
+    user = Repo.get_by(User, phone: String.downcase(phone))
+
+    case check_password(user, password) do
+      true -> {:ok, user}
+      _ -> :error
+    end
+  end
+
+  defp check_password(user, password) do
+    case user do
+      nil -> false
+      _ -> Comeonin.Bcrypt.checkpw(password, user.encrypted_password)
+    end
+  end
+  
+end
