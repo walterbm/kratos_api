8f819a442a8acf50174e5032296e3864858d948d
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-09-09 00:52:27 -0400

    add district lookup in registration endpoint

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -6,6 +6,14 @@ defmodule KratosApi.SessionView do
   end
 
   def render("error.json", _) do
-    %{error: "Invalid email or password"}
+    %{error: "Invalid phone number or password"}
+  end
+
+  def render("delete.json", _) do
+    %{ok: true}
+  end
+
+  def render("forbidden.json", %{error: error}) do
+    %{error: error}
   end
 end
