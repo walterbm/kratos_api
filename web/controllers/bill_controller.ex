444feab3b7259869eb7853a03bdc253786a774f6
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-10-24 01:43:07 -0400

    update bill controller to return image, state, and party for all sponsors and co-sponsors

--- web/controllers/bill_controller.ex
+++ web/controllers/bill_controller.ex
@@ -4,7 +4,22 @@ defmodule KratosApi.BillController do
   plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
 
   def show(conn, %{"id" => id}) do
-    json conn, Govtrack.bill(id).body
+    bill = Govtrack.bill(id).body
+
+    extract_party_and_state = ~r/\[(?<party>[A-Z])-(?<state>\w+)/
+
+    sponsor_update =
+      Map.put(bill["sponsor"], "image", "#{Application.get_env(:kratos_api, :assets_url)}/225x275/#{bill["sponsor"]["bioguideid"]}.jpg")
+      |> Map.put("state", Regex.named_captures(extract_party_and_state, bill["sponsor"]["name"])["state"])
+      |> Map.put("party", Regex.named_captures(extract_party_and_state, bill["sponsor"]["name"])["party"])
+
+    cosponsors_update =
+      Enum.map(bill["cosponsors"], &(Map.put(&1, "image","#{Application.get_env(:kratos_api, :assets_url)}/225x275/#{&1["bioguideid"]}.jpg")))
+      |> Enum.map(&(Map.put(&1, "state", Regex.named_captures(extract_party_and_state, &1["name"])["state"])))
+      |> Enum.map(&(Map.put(&1, "party", Regex.named_captures(extract_party_and_state, &1["name"])["party"])))
+
+    updated_bill = bill |> Map.merge(%{"sponsor" => sponsor_update}) |> Map.merge(%{"cosponsors" => cosponsors_update})
+    json conn, updated_bill
   end
 
 end
