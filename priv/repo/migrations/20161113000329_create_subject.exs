3cbd8ea51d480482dbeae12d6afb0eee26d28dee
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-05 23:28:19 -0500

    re-write Bill model & migration to match GPO

---
+++ priv/repo/migrations/20161113000329_create_subject.exs
@@ -0,0 +1,12 @@
+defmodule KratosApi.Repo.Migrations.CreateSubject do
+  use Ecto.Migration
+
+  def change do
+    create table(:subjects) do
+      add :name, :string
+
+      timestamps()
+    end
+
+  end
+end
--- web/models/bill.ex
+++ web/models/bill.ex
@@ -3,49 +3,46 @@ defmodule KratosApi.Bill do
 
   schema "bills" do
 
-    field :govtrack_id, :integer
-    field :link, :string
-    field :title_without_number, :string
-    field :is_current, :boolean
-    field :lock_title, :boolean
-    field :senate_floor_schedule_postdate, Ecto.Date
-    field :sliplawnum, :integer
-    field :bill_type, :string
-    field :docs_house_gov_postdate, Ecto.Date
-    field :noun, :string
-    field :current_status_date, Ecto.Date
-    field :source_link, :string
-    field :current_status, :string
-    field :introduced_date, Ecto.Date
-    field :bill_type_label, :string
-    field :bill_resolution_type,:string
-    field :display_number, :string
-    field :title, :string
-    field :sliplawpubpriv, :string
-    field :current_status_label, :string
-    field :is_alive, :boolean
-    field :number, :integer
-    field :source, :string
-    field :current_status_description, :string
-    field :titles, {:array, {:array, :string}}
-    field :major_actions, {:array, {:array, :any}}
+    field :actions, {:array, {:array, :any}}
+    field :amendments, {:array, :map}
+    field :gpo_id, :string
+    field :type, :string
+    field :committee_history, {:array, :map}
+    field :enacted_as, :string
+    field :active, :boolean
+    field :awaiting_signature, :boolean
+    field :enacted, :boolean
+    field :vetoed, :boolean
+    field :history, :map
+    field :introduced_at, Ecto.Date
+    field :number, :string
+    field :official_title, :string
+    field :popular_title, :string
     field :related_bills, {:array, :map}
+    field :short_title, :string
+    field :status, :string
+    field :status_at, Ecto.Date
+    field :top_term, :string
+    field :summary_text, :string
+    field :summary_date, Ecto.DateTime
+    field :titles, {:array, {:array, :string}}
+    field :gpo_data_updated_at, Ecto.DateTime
+    field :pdf_url, :string
 
     belongs_to :congress_number, KratosApi.CongressNumber, references: :number
-    belongs_to :sponsor, KratosApi.Role, references: :role_id
+    belongs_to :sponsor, KratosApi.Role
 
     many_to_many :committees, KratosApi.Committee, join_through: "bill_committees"
     many_to_many :cosponsors, KratosApi.Role, join_through: "bill_cosponsors"
-    many_to_many :terms, KratosApi.Term, join_through: "bill_terms"
+    many_to_many :subjects, KratosApi.Term, join_through: "bill_subjects"
 
     timestamps()
   end
 
   @required_fields ~w(govtrack_id)
-  @optional_fields ~w(link title_without_number is_current lock_title senate_floor_schedule_postdate sliplawnum bill_type
-    docs_house_gov_postdate noun current_status_date source_link current_status introduced_date bill_type_label
-    bill_resolution_type display_number title sliplawpubpriv current_status_label is_alive number source
-    current_status_description titles major_actions related_bills)
+  @optional_fields ~w(actions amendments gpo_id type committee_history enacted_as active awaiting_signature
+    enacted vetoed history introduced_at number official_title popular_title related_bills short_title
+    status status_at top_term summary_text summary_date titles gpo_data_updated_at pdf_url)
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
@@ -53,16 +50,6 @@ defmodule KratosApi.Bill do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)
-  end
-
-  def find_or_mark(bill_id, caller, caller_id) do
-    role = KratosApi.Bill |> KratosApi.Repo.get_by(govtrack_id: bill_id)
-    if !role do
-      KratosApi.Repo.insert(%KratosApi.MissingData{type: "bill", govtrack_id: bill_id, caller: caller, caller_id: caller_id})
-      nil
-    else
-      role
-    end
   end
 
 end
