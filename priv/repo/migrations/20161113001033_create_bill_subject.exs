aa2c7d4b063dbf39ddf0a33b01d3d1d6e37f000f
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-12 02:03:09 -0500

    remove timestamps from all join-tables

--- priv/repo/migrations/20161113001033_create_bill_subject.exs
+++ priv/repo/migrations/20161113001033_create_bill_subject.exs
@@ -5,8 +5,6 @@ defmodule KratosApi.Repo.Migrations.CreateBillSubject do
     create table(:bill_subjects) do
       add :bill_id, references(:bills, on_delete: :nothing)
       add :subject_id, references(:subjects, on_delete: :nothing)
-
-      timestamps()
     end
     create index(:bill_subjects, [:bill_id])
     create index(:bill_subjects, [:subject_id])
