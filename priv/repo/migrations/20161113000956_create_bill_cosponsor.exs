9ffa4873683d3621279d2a5e22004ef30a0b055c
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-12 02:04:46 -0500

    update Bill model & migration

--- priv/repo/migrations/20161113000956_create_bill_cosponsor.exs
+++ priv/repo/migrations/20161113000956_create_bill_cosponsor.exs
@@ -4,9 +4,7 @@ defmodule KratosApi.Repo.Migrations.CreateBillCosponsor do
   def change do
     create table(:bill_cosponsors) do
       add :bill_id, references(:bills, on_delete: :nothing)
-      add :cosponsor_id, references(:roles, on_delete: :nothing)
-
-      timestamps()
+      add :cosponsor_id, references(:persons, on_delete: :nothing)
     end
     create index(:bill_cosponsors, [:bill_id])
     create index(:bill_cosponsors, [:cosponsor_id])
