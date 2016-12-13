68c8c9117d3b7ac5e67af3c221ca14bc281ed1dd
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-12 23:46:33 -0500

    fix foreign key in bill_committees join table

--- priv/repo/migrations/20161113000807_create_bill_committee.exs
+++ priv/repo/migrations/20161113000807_create_bill_committee.exs
@@ -4,10 +4,10 @@ defmodule KratosApi.Repo.Migrations.CreateBillCommittee do
   def change do
     create table(:bill_committees) do
       add :bill_id, references(:bills, on_delete: :nothing)
-      add :committee_code, references(:committees, on_delete: :nothing, column: :code, type: :string)
+      add :committee_id, references(:committees, on_delete: :nothing)
     end
     create index(:bill_committees, [:bill_id])
-    create index(:bill_committees, [:committee_code])
+    create index(:bill_committees, [:committee_id])
 
   end
 end
