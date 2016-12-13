2f8640a916a6fd27f8ae55d893e70bbb90ecf65c
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-12 23:51:13 -0500

    update bill_controller to pull directly from database

--- web/views/person_view.ex
+++ web/views/person_view.ex
@@ -1,7 +1,17 @@
 defmodule KratosApi.PersonView do
   use KratosApi.Web, :view
 
+  def render("persons.json", %{persons: persons}) do
+    %{data: render_many(persons, KratosApi.PersonView, "person.json", as: :person)}
+  end
+
   def render("person.json", %{person: person}) do
+    roles =
+      case person.roles do
+        %Ecto.Association.NotLoaded{} -> []
+        _ -> person.roles
+      end
+
     %{
       govtrack_id: person.govtrack_id,
       cspanid: person.cspanid,
@@ -14,6 +24,8 @@ defmodule KratosApi.PersonView do
       link: person.link,
       middlename: person.middlename,
       name: person.name,
+      current_party: person.current_party,
+      current_state: person.current_state,
       namemod: person.namemod,
       nickname: person.nickname,
       osid: person.osid,
@@ -21,7 +33,8 @@ defmodule KratosApi.PersonView do
       sortname: person.sortname,
       twitterid: person.twitterid,
       youtubeid: person.youtubeid,
-      image_url: person.image_url
+      image_url: person.image_url,
+      roles: render_many(roles, KratosApi.RoleView, "role.json")
     }
   end
 
