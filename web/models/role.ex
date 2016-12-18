88fe200ae76502b7d0ac6a316058d00bc93041ce
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-18 11:55:09 -0500

    remove find_or_mark/1 from Role model

--- web/models/role.ex
+++ web/models/role.ex
@@ -1,6 +1,11 @@
 defmodule KratosApi.Role do
   use KratosApi.Web, :model
 
+  alias KratosApi.{
+    Repo,
+    Role
+  }
+
   schema "roles" do
 
     field :current, :boolean, default: false
@@ -45,23 +50,13 @@ defmodule KratosApi.Role do
     |> cast(params, @required_fields, @optional_fields)
   end
 
-  def find_or_mark(role_id, caller, caller_id) do
-    role = KratosApi.Role |> KratosApi.Repo.get_by(govtrack_id: role_id)
-    if !role do
-      KratosApi.Repo.insert(%KratosApi.MissingData{type: "role", govtrack_id: role_id, caller: caller, caller_id: caller_id})
-      nil
-    else
-      role
+  def find_or_create(data) do
+    case Repo.get_by(Role, govtrack_id: data["id"]) do
+      nil ->
+        KratosApi.Sync.Role.save(data)
+        nil
+      role -> role
     end
   end
 
-  def find_or_create(data) do
-    role = KratosApi.Role |> KratosApi.Repo.get_by(govtrack_id: data["id"])
-    if !role do
-      KratosApi.Sync.Role.save(data)
-      nil
-    else
-      role
-    end
-  end
 end
