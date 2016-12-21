f3ef6fe8eb3d3517719ebb576add14ef7b04058e
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-20 23:26:23 -0500

    flip person/role nesting in representatives#show

--- web/controllers/district_controller.ex
+++ web/controllers/district_controller.ex
@@ -1,17 +1,20 @@
 defmodule KratosApi.DistrictController do
   use KratosApi.Web, :controller
 
-  plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
+  #plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
 
   def show(conn, params) do
-    query = from r in KratosApi.Role,
+    query = from p in KratosApi.Person,
+      join: r in KratosApi.Role,
+      where: r.person_id == p.id,
       where: r.state == ^String.upcase(params["state"]),
       where: r.district == ^params["id"] or r.role_type ==  "senator",
-      preload: [:person, :congress_numbers]
+      preload: [:roles]
+
 
     {representatives, kerosene} = query |> KratosApi.Repo.paginate(params)
 
-    render(conn, "roles.json", roles: representatives, kerosene: kerosene)
+    render(conn, "representatives.json", representatives: representatives, kerosene: kerosene)
   end
 
 end
