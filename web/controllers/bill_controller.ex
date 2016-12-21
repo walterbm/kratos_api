300f0bce081e0890c35b45fda6b9f5ea66b02720
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-12-20 22:22:04 -0500

    add auth and 404 handlers for Bill and Tally controllers

--- web/controllers/bill_controller.ex
+++ web/controllers/bill_controller.ex
@@ -1,11 +1,11 @@
 defmodule KratosApi.BillController do
   use KratosApi.Web, :controller
 
-  # plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
+  plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
 
   def show(conn, %{"id" => id}) do
     bill =
-      KratosApi.Repo.get(KratosApi.Bill, id)
+      KratosApi.Repo.get!(KratosApi.Bill, id)
       |> KratosApi.Repo.preload([:subjects, :sponsor, :cosponsors, :committees, :related_bills, :tallies])
       |> KratosApi.Repo.preload(sponsor: :roles)
       |> KratosApi.Repo.preload(cosponsors: :roles)
