18ef4290c06e6b1da7522293be89ce63f9b6aeed
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-03 23:07:11 -0500

    add light person info to tallies#show view

--- web/views/vote_view.ex
+++ web/views/vote_view.ex
@@ -5,11 +5,7 @@ defmodule KratosApi.VoteView do
     %{
       id: vote.id,
       value: vote.value,
-      person_firstname: vote.person.firstname,
-      person_lastname: vote.person.lastname,
-      person_current_party: vote.person.current_party,
-      person_current_state: vote.person.current_state,
-      person_image: vote.person.image_url
+      person: render_one(vote.person, KratosApi.PersonView, "person_light.json")
     }
   end
 
