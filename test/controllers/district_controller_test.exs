11a07a9a3cc70a8d9caa72223e4409202a0927a1
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-03 23:36:54 -0500

    fix district_controller_test for flipped person/role in district#show view

--- test/controllers/district_controller_test.exs
+++ test/controllers/district_controller_test.exs
@@ -17,9 +17,30 @@ defmodule KratosApi.DistrictControllerTest do
       |> get("/api/districts/mo/1")
 
     one = json_response(conn, 200)["data"] |> List.first
-    assert one["description"] == "Junior Senator from Missouri"
-    assert one["govtrack_id"] == 268
-    assert one["person"]["lastname"] == "Blunt"
-    assert one["congress_numbers"] == [112,113,114]
+    assert one["lastname"] == "Blunt"
+    assert one["firstname"] == "Roy"
+    assert one["bioguideid"] == "B000575"
+    assert one["cspanid"] == 45465
+    assert one["current_party"] == "R"
+    assert one["current_state"] == "MO"
+    assert one["image_url"] == "/225x275/B000575.jpg"
+    assert one["name"] == "Sen. Roy Blunt [R-MO]"
+    assert one["osid"] == "N00005195"
+    assert one["pvsid"] == "418"
+
+    one_role = one["roles"] |> List.first
+    assert one_role["description"] == "Junior Senator from Missouri"
+    assert one_role["current"] == true
+    assert one_role["party"] == "Republican"
+    assert one_role["role_type"] == "senator"
+    assert one_role["role_type_label"] == "Senator"
+    assert one_role["senator_class"] == "class3"
+    assert one_role["senator_class_label"] == "Class 3"
+    assert one_role["senator_rank"] == "junior"
+    assert one_role["senator_rank_label"] == "Junior"
+    assert one_role["state"] == "MO"
+    assert one_role["title"] == "Sen."
+    assert one_role["title_long"] == "Senator"
+    assert one_role["website"] == "http://www.blunt.senate.gov"
   end
 end
