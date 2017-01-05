db987bc12a6a262ce7db64987037970f0abef2b6
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-05 01:55:57 -0500

    add CRUD controller actions for UserVote

--- web/views/vote_view.ex
+++ web/views/vote_view.ex
@@ -1,5 +1,6 @@
 defmodule KratosApi.VoteView do
   use KratosApi.Web, :view
+  import Kerosene.JSON
 
   def render("vote.json", %{vote: vote}) do
     %{
@@ -14,6 +15,15 @@ defmodule KratosApi.VoteView do
       id: vote.id,
       value: vote.value,
       tally: render_one(vote.tally, KratosApi.TallyView, "tally_flat.json"),
+    }
+  end
+
+  def render("user_votes.json", %{user_votes: user_votes, kerosene: kerosene, conn: conn}) do
+    %{
+      data: %{
+        voting_record: render_many(user_votes, KratosApi.VoteView, "vote_record.json", as: :vote),
+      },
+      pagination: paginate(conn, kerosene)
     }
   end
 
