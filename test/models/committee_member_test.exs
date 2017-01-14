b4b9fda4deb3530f167d93dc4ad253c7eb033c7f
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-14 18:15:15 -0500

    fix tests for new Person, Committee, Term models

--- test/models/committee_member_test.exs
+++ test/models/committee_member_test.exs
@@ -1,18 +1,18 @@
-defmodule KratosApi.CommiteeMemberTest do
+defmodule KratosApi.CommitteeMemberTest do
   use KratosApi.ModelCase
 
-  alias KratosApi.CommiteeMember
+  alias KratosApi.CommitteeMember
 
   @valid_attrs %{title: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
-    changeset = CommiteeMember.changeset(%CommiteeMember{}, @valid_attrs)
+    changeset = CommitteeMember.changeset(%CommitteeMember{}, @valid_attrs)
     assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
-    changeset = CommiteeMember.changeset(%CommiteeMember{}, @invalid_attrs)
+    changeset = CommitteeMember.changeset(%CommitteeMember{}, @invalid_attrs)
     refute changeset.valid?
   end
 end
