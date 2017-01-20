47d9196c0e229c84b77b786242c59e71589c968f
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-20 02:50:03 -0500

    add VoteController and Vote#index

---
+++ web/controllers/vote_controller.ex
@@ -0,0 +1,19 @@
+defmodule KratosApi.VoteController do
+  use KratosApi.Web, :controller
+
+  plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
+
+  def index(conn, params) do
+    query = from v in KratosApi.Vote,
+      where: v.person_id == ^params["id"],
+      join: t in KratosApi.Tally,
+      where: v.tally_id == t.id,
+      order_by: [desc: t.date],
+      preload: [:tally]
+
+    {voting_records, kerosene} = query |> KratosApi.Repo.paginate(params)
+
+    render(conn, KratosApi.PersonView, "voting_records.json", voting_records: voting_records, kerosene: kerosene)
+  end
+
+end
