4e2c825f9eafbd8a4e4cf09bacc31a32bc89abd5
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-22 22:55:30 -0500

    add 404.json error view

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -5,6 +5,10 @@ defmodule KratosApi.ErrorView do
     "Page not found"
   end
 
+  def render("404.json", _assigns) do
+    %{ error: "resource not found" }
+  end
+
   def render("500.html", _assigns) do
     "Internal server error"
   end
