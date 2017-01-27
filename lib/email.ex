9f008a549204356b5a411677e139f4ab70ec4f25
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-27 02:41:58 -0500

    fix documentation bug and text email bug

--- lib/email.ex
+++ lib/email.ex
@@ -8,7 +8,7 @@ defmodule Email do
     base_email()
     |> to(email_address)
     |> subject("Kratos Account Recovery")
-    |> text_body("Forgot Your Kratos Password? Copy this url to reset: #{@url <> "/reset-password?reset_token="}#{@reset_token}")
+    |> text_body("Forgot Your Kratos Password? Copy this url to reset: #{@url <> "/reset-password?reset_token="}#{reset_token}")
     |> render("forgot_password.html", reset_token: reset_token, url: @url <> "/reset-password?reset_token=")
   end
 
