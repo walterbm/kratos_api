0008bb236ac9a68b44a4bb441cc53327bacafb62
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-26 23:48:23 -0500

    automatically update district, state, zip, city when address is updated

--- web/helpers/find_district.ex
+++ web/helpers/find_district.ex
@@ -6,8 +6,15 @@ defmodule KratosApi.FindDistrict do
     geocodio_response =
       HTTPotion.get(@geocodio[:api_url], query: %{q: "#{address} #{city} #{state} #{zip}", api_key: @geocodio[:api_key], fields: "cd"}).body
       |> Poison.decode!
-
-    %{"district" => List.first(geocodio_response["results"])["fields"]["congressional_district"]["district_number"]}
+      |> Map.get("results")
+      |> List.first
+      
+    %{
+      "district" => geocodio_response["fields"]["congressional_district"]["district_number"],
+      "state" => geocodio_response["address_components"]["state"],
+      "city" => geocodio_response["address_components"]["city"],
+      "zip" => geocodio_response["address_components"]["zip"],
+    }
   end
 
   def by_address(_), do: %{}
