1331614e075215d3704aed68aafe6e4d3f018742
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-01-31 22:05:48 -0500

    refactor forgot passoword controller and downcase input email

--- web/controllers/forgot_password_controller.ex
+++ web/controllers/forgot_password_controller.ex
@@ -11,12 +11,8 @@ defmodule KratosApi.ForgotPasswordController do
   @token_gen Application.get_env(:kratos_api, :token_gen)
 
   def forgot_password(conn, %{"email" => email}) do
-    reset_token = %{email: email}
-      |> @token_gen.token
-      |> @token_gen.with_exp
-      |> @token_gen.with_signer(@token_gen.hs256(Application.get_env(:joken, :secret_key)))
-      |> @token_gen.sign
-      |> @token_gen.get_compact
+    email = String.downcase(email)
+    reset_token = generate_token(email)
 
     Email.forgot_password_email(email, reset_token) |> Mailer.deliver_now
 
@@ -30,16 +26,11 @@ defmodule KratosApi.ForgotPasswordController do
   end
 
   def new_password(conn, %{"reset_token" => reset_token, "password" => password}) do
-    verified_token = reset_token
-      |> @token_gen.token
-      |> @token_gen.with_validation("exp", &(&1 > Time.current))
-      |> @token_gen.with_signer(@token_gen.hs256(Application.get_env(:joken, :secret_key)))
-      |> @token_gen.verify!
-
-    case verified_token do
+    case verify_token(reset_token) do
       {:ok, claims} ->
-        user = Repo.get_by(User, email: claims["email"])
-        changeset = User.reset_password_changeset(user, %{password: password})
+        changeset =
+          Repo.get_by(User, email: claims["email"])
+          |> User.reset_password_changeset(%{password: password})
 
         case Repo.update(changeset) do
           {:ok, _user} ->
@@ -60,6 +51,23 @@ defmodule KratosApi.ForgotPasswordController do
          |> put_layout(false)
          |> render("reset_password.html", reset_token: reset_token)
     end
+  end
+
+  defp generate_token(email) do
+    %{email: email}
+      |> @token_gen.token
+      |> @token_gen.with_exp
+      |> @token_gen.with_signer(@token_gen.hs256(Application.get_env(:joken, :secret_key)))
+      |> @token_gen.sign
+      |> @token_gen.get_compact
+  end
+
+  defp verify_token(reset_token) do
+    reset_token
+      |> @token_gen.token
+      |> @token_gen.with_validation("exp", &(&1 > Time.current))
+      |> @token_gen.with_signer(@token_gen.hs256(Application.get_env(:joken, :secret_key)))
+      |> @token_gen.verify!
   end
 
 end
