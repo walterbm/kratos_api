57dfb113298508ce2cd49ea02b9df4d275daa9b6
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-02-07 20:31:54 -0500

    add slack notifications

---
+++ lib/jobs/slack_notification.ex
@@ -0,0 +1,9 @@
+defmodule KratosApi.Slack do
+
+  @slack_url Application.get_env(:kratos_api, :slack_url)
+
+  def notify(message) do
+    HTTPotion.get(@slack_url <> "&text=" <> URI.encode(message))
+  end
+
+end
