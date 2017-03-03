85a725a972c87d3c30f1eeccf3bf74a87d74eef6
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-03-02 23:42:03 -0500

    improve session scrape and sync congressional recess dates

--- lib/sync/sync_session.ex
+++ lib/sync/sync_session.ex
@@ -3,7 +3,8 @@ defmodule KratosApi.Sync.Session do
   @remote_scrape Application.get_env(:kratos_api, :remote_scraper)
 
   @congress_session_source %{
-    url: "https://www.senate.gov/legislative/2017_schedule.htm",
+    base_url: "https://www.senate.gov/legislative/",
+    route: "_schedule.htm",
     dom_node: "table"
   }
 
@@ -22,13 +23,57 @@ defmodule KratosApi.Sync.Session do
     "Dec": "12",
   }
 
-  def sync() do
-    @remote_scrape.scrape(@congress_session_source.url, @congress_session_source.dom_node)
+  alias KratosApi.{
+    Repo,
+    CongressionalRecess
+  }
+
+  def sync(), do: sync(Date.utc_today() |> Map.get(:year))
+  def sync(year) do
+    @remote_scrape.scrape(page(year), @congress_session_source.dom_node)
+    |> scrape_dates
+    |> extract_dates
+    |> Enum.map(&(convert_date(&1, year)))
+    |> Enum.map(&(save(&1, year)))
+  end
+
+  defp page(year) do
+     "#{@congress_session_source.base_url}#{year}#{@congress_session_source.route}"
+  end
+
+  defp scrape_dates(html) do
+    html
     |> List.first
     |> elem(2)
     |> Enum.map(fn tr -> elem(tr, 2) |> List.first |> elem(2) end)
     |> List.flatten
+  end
+
+  defp extract_dates(date_string_array) do
+    date_string_array
     |> List.delete("Date")
+    |> Enum.map(&(String.split(&1, " - ")))
+  end
+
+  defp convert_date([single_date], year) do
+    date = convert(single_date, year)
+    [date, date]
+  end
+  defp convert_date(date_range, year) do
+    Enum.map(date_range, &(convert(&1, year)))
+  end
+
+  defp convert(date, year) do
+    [month, day] = String.split(date)
+    Date.from_iso8601!("#{year}-#{Map.get(@months, String.to_atom(month))}-#{String.pad_leading(day, 2, "0")}")
+  end
+
+  defp save([start_date, end_date], year) do
+    %CongressionalRecess{
+      start_date: start_date,
+      end_date: end_date,
+      year: year,
+    } |> Repo.insert!
   end
 
 end
