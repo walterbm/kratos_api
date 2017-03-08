ca11b212527ed65ee2945d1b3971b5c6d3dcf3ae
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-03-07 23:55:17 -0500

    update prod and stage config to include remote scraper

--- config/prod.exs
+++ config/prod.exs
@@ -24,6 +24,7 @@ config :kratos_api, :govtrack_api, Govtrack
 config :kratos_api, :remote_queue, KratosApi.RemoteQueue
 config :kratos_api, :remote_storage, KratosApi.RemoteStorage
 config :kratos_api, :remote_service, KratosApi.RemoteService
+config :kratos_api, :remote_scraper, KratosApi.RemoteScrape
 config :kratos_api, :token_gen, KratosApi.TokenGen
 config :kratos_api, :slack, KratosApi.Slack
 config :kratos_api, :url, "https://kratos.site"
