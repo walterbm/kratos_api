1b71640124c9370eef84597c95787f67f5c41f56
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-03-09 00:06:25 -0500

    add /state/:state/image endpoint

--- web/controllers/state_controller.ex
+++ web/controllers/state_controller.ex
@@ -1,19 +1,36 @@
 defmodule KratosApi.StateController do
   use KratosApi.Web, :controller
 
+  alias KratosApi.{
+    Repo,
+    Person,
+    StateImage
+  }
+
   plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
 
   def show(conn, %{ "state" => state }) do
-    query = from p in KratosApi.Person,
+    query = from p in Person,
       where: p.is_current == true,
       where: p.current_state == ^String.upcase(state),
       distinct: p.current_district,
       where: not is_nil(p.current_district),
       select: p.current_district
 
-    districts = KratosApi.Repo.all(query)
+    districts = Repo.all(query)
 
     json conn, %{ districts: districts }
+  end
+
+  def image(conn, %{ "state" => state }) do
+     case Repo.get(StateImage, String.upcase(state)) do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> render(KratosApi.ErrorView, "404.json")
+      state ->
+        json conn, %{ url: state.image_url }
+    end
   end
 
 end
