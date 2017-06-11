f6b3af2acb820d83562b3938694b61d241a4fa41
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-06-11 00:12:32 -0400

    add environment to release config

--- rel/config.exs
+++ rel/config.exs
@@ -15,6 +15,27 @@ use Mix.Releases.Config,
 # For a full list of config options for both releases
 # and environments, visit https://hexdocs.pm/distillery/configuration.html
 
+
+# You may define one or more environments in this file,
+# an environment's settings will override those of a release
+# when building in that environment, this combination of release
+# and environment configuration is called a profile
+
+environment :dev do
+  set dev_mode: true
+  set include_erts: false
+end
+
+environment :stage do
+  set include_erts: true
+  set include_src: false
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+end
+
 # You may define one or more releases in this file.
 # If you have not set a default release, or selected one
 # when running `mix release`, the first release in the file
