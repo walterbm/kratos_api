c4fba61f83f41e57ddd95ff41c3c895f26082a93
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-06-19 22:31:13 -0400

    add scraper for house and senate 'on the floor today'

--- lib/sync/remote_scrape.ex
+++ lib/sync/remote_scrape.ex
@@ -1,9 +1,16 @@
 defmodule KratosApi.RemoteScrape do
 
-  def scrape(url, tag) do
+  def scrape(url, tag), do: scrape(:html, url, tag)
+  def scrape(:html, url, tag) do
     HTTPoison.get!(url, [], hackney: [:insecure])
     |> Map.get(:body)
     |> Floki.find(tag)
+  end
+  def scrape(:xml, url, mapping) do
+    HTTPoison.get!(url, [])
+    |> Map.get(:body)
+    |> SweetXml.parse
+    |> SweetXml.xmap(mapping)
   end
 
 end
