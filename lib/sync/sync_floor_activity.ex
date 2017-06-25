ad5fd7af7e39c22f2189b6f27bb65f57f4236d42
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-06-24 23:54:55 -0400

    finish basic house floor activity sync

--- lib/sync/sync_floor_activity.ex
+++ lib/sync/sync_floor_activity.ex
@@ -1,6 +1,12 @@
 defmodule KratosApi.Sync.Floor do
   import SweetXml
 
+  alias KratosApi.{
+    Repo,
+    SyncHelpers,
+    FloorActivity
+  }
+
   @remote_scrape Application.get_env(:kratos_api, :remote_scraper)
 
   @congress_on_the_floor_source %{
@@ -20,26 +26,40 @@ defmodule KratosApi.Sync.Floor do
       title: ~x"./title/text()"s,
       description: ~x"./description/text()"s,
       link: ~x"./link/text()"s,
+      day: ~x"./pubDate/text()"s,
     ]}]
   }
 
   def sync() do
-    sync(:senate)
+    # sync(:senate)
     sync(:house)
   end
 
   def sync(chamber) do
     @remote_scrape.scrape(:xml, url(chamber), Map.get(@mapping, chamber))
     |> Map.get(:on_the_floor)
+    |> Enum.map(&(stage(chamber, &1)))
     |> Enum.each(&save/1)
   end
 
-  def url(chamber) do
+  defp url(chamber) do
     Map.get(@congress_on_the_floor_source, chamber)
   end
 
-  def save(record) do
-    IO.inspect record
+  defp stage(:house, %{title: title, description: description, link: link, day: day}) do
+    %{
+      link: link,
+      title: title,
+      chamber: "house",
+      description: description,
+      published_at: SyncHelpers.convert_datetime(day, "RFC1123"),
+      md5: SyncHelpers.gen_md5(title <> description)
+    }
+  end
+
+  defp save(params) do
+    FloorActivity.changeset(%FloorActivity{}, params)
+    |> Repo.insert!
   end
 
 end
