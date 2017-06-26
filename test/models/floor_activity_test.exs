b25314fd36adbed6c26d37c1971276f73eab955a
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-06-25 22:46:04 -0400

    finish house floor activity sync

--- test/models/floor_activity_test.exs
+++ test/models/floor_activity_test.exs
@@ -3,7 +3,7 @@ defmodule KratosApi.FloorActivityTest do
 
   alias KratosApi.FloorActivity
 
-  @valid_attrs %{chamber: "some content", day: %{day: 17, month: 4, year: 2010}, description: "some content", link: "some content", title: "some content"}
+  @valid_attrs %{chamber: "some content", published_at: %{day: 23, month: 6, usec: 0, year: 2017, hour: 10, min: 43, sec: 9}, description: "some content", link: "some content", title: "some content", md5: "cabbage"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
