0bc354f3bc2aaa42d017ea448f66ac6f15a31aed
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-09-17 22:36:40 -0400

    return subject#index in alphabetical order

--- web/controllers/subject_controller.ex
+++ web/controllers/subject_controller.ex
@@ -4,7 +4,9 @@ defmodule KratosApi.SubjectController do
   plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
 
   def index(conn, _params) do
-    subjects = KratosApi.Repo.all(KratosApi.Subject)
+    subjects =
+      KratosApi.Repo.all(KratosApi.Subject)
+      |> Enum.sort(&(&1.name <= &2.name))
 
     render(conn, KratosApi.SubjectView, "subjects.json", subjects: subjects)
   end
