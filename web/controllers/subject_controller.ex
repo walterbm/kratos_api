ec52978be3ac9a5f5bb7f291278bc58cbc0b443e
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-09-23 16:39:55 -0400

    add query param to subject#index to return only active subjects

--- web/controllers/subject_controller.ex
+++ web/controllers/subject_controller.ex
@@ -1,14 +1,34 @@
 defmodule KratosApi.SubjectController do
   use KratosApi.Web, :controller
 
+  alias KratosApi.{
+    Repo,
+    Bill,
+    Subject,
+    SubjectView
+  }
+
   plug Guardian.Plug.EnsureAuthenticated, handler: KratosApi.SessionController
+
+  def index(conn, %{"active" => "true"}) do
+    query = from bill in Bill,
+      distinct: bill.top_subject_id,
+      join: subject in Subject,
+      on: subject.id == bill.top_subject_id,
+      order_by: subject.name,
+      select: subject
+
+    subjects = Repo.all(query)
+
+    render(conn, SubjectView, "subjects.json", subjects: subjects)
+  end
 
   def index(conn, _params) do
     subjects =
-      KratosApi.Repo.all(KratosApi.Subject)
+      Repo.all(Subject)
       |> Enum.sort(&(&1.name <= &2.name))
 
-    render(conn, KratosApi.SubjectView, "subjects.json", subjects: subjects)
+    render(conn, SubjectView, "subjects.json", subjects: subjects)
   end
 
 end
