300dfa84f21054ace45c88a27a432cda5243810b
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-10-29 19:07:14 -0400

    refactor the remote district lookup to handle D.C.

--- web/helpers/remote_district_lookup.ex
+++ web/helpers/remote_district_lookup.ex
@@ -5,27 +5,20 @@ defmodule KratosApi.FindDistrict do
   def by_query(query) do
     geocodio_response = search(query)
 
-    if geocodio_response["fields"]["congressional_district"]["district_number"] do
+    if found_district?(geocodio_response) do
       district = geocodio_response["fields"]["congressional_district"]["district_number"]
       state = geocodio_response["address_components"]["state"]
       {:ok, {state, district}}
     else
       {:error, geocodio_response}
     end
-
   end
 
   def by_address(%{"address" => address, "city" => city, "state" => state, "zip" => zip}) do
     geocodio_response = search("#{address} #{city} #{state} #{zip}")
 
-    if geocodio_response["fields"]["congressional_district"]["district_number"] do
-      if geocodio_response["address_components"]["county"] == "District of Columbia" do
-        {:ok, address_fields_map(%{
-          geocodio_response | "fields" => %{"congressional_district" => %{"district_number" => 0}}
-        })}
-      else
-        {:ok, address_fields_map(geocodio_response)}
-      end
+    if found_district?(geocodio_response) do
+      {:ok, address_fields_map(geocodio_response)}
     else
       {:error, geocodio_response}
     end
@@ -46,6 +39,16 @@ defmodule KratosApi.FindDistrict do
     |> Poison.decode!
     |> Map.get("results")
     |> List.first
+    |> handle_district_of_columbia
+  end
+
+  defp handle_district_of_columbia(response = %{"address_components" => %{"county" => "District of Columbia" }}) do
+    %{response | "fields" => %{"congressional_district" => %{"district_number" => 0}}}
+  end
+  defp handle_district_of_columbia(response), do: response
+
+  defp found_district?(response) do
+    !!get_in(response, ["fields", "congressional_district", "district_number"])
   end
 
 end
