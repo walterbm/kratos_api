2ac093d4a888e98dec1e6e8eb846b29d474b98cd
Walter Beller-Morales <walter.beller.morales@gmail.com>
2019-08-24 21:45:33 -0400

    clean up config and tests stubs

--- test/support/remote_search_in_memory.ex
+++ test/support/remote_search_in_memory.ex
@@ -13,7 +13,7 @@ defmodule KratosApi.Search.Remote.InMemory do
       headers: [{"Access-Control-Allow-Origin", "*"},
        {"Content-Type", "application/json; charset=UTF-8"},
        {"Content-Length", "355"}, {"Connection", "keep-alive"}],
-      request_url: "search-kratos-bcmcpmthjc7jevxwmc23kt5qjy.us-east-1.es.amazonaws.com/congress/bill/_search",
+      request_url: "test-host/congress/bill/_search",
       status_code: 404}}
   end
 
